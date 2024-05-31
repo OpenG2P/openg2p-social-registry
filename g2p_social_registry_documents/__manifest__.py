46ea46ce8d93286d0af1fc16addafe3ee224b4a5
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2024-05-31 10:15:42 +0300

    Add starter code for OpenG2P SR Documents module

---
+++ g2p_social_registry_documents/__manifest__.py
@@ -0,0 +1,22 @@
+# Part of OpenG2P. See LICENSE file for full copyright and licensing details.
+{
+    "name": "OpenG2P Social Registry: Documents",
+    "category": "G2P",
+    "version": "17.0.1.0.0",
+    "sequence": 1,
+    "author": "OpenG2P",
+    "website": "https://openg2p.org",
+    "license": "Other OSI approved licence",
+    "development_status": "Alpha",
+    "depends": [
+        "g2p_documents",
+        "g2p_social_registry",
+    ],
+    "data": [
+    ],
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
