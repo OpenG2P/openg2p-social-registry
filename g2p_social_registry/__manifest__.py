77c5b65259ffe7ff80b1c32331ce70f1cc645f2b
Shibu Narayanan <104964065+shibu-narayanan@users.noreply.github.com>
2025-03-03 00:35:31 +0530

    Revert "Move develop changes to 1.5"

--- g2p_social_registry/__manifest__.py
+++ g2p_social_registry/__manifest__.py
@@ -18,7 +18,6 @@
         "views/main_view.xml",
         "views/region.xml",
         "views/registrant_view.xml",
-        "views/group_individual_search_extend.xml",
     ],
     "assets": {},
     "demo": [],
--- g2p_social_registry_model/models/__init__.py
+++ g2p_social_registry_model/models/__init__.py
@@ -1,6 +1,4 @@
 # Part of OpenG2P. See LICENSE file for full copyright and licensing details.
 
-from . import odk_import
+from . import odk_client
 from . import res_partner
-from . import process_group_mixin
-from . import process_individual_mixin
